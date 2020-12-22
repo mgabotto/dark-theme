c97927317d0ea16ddb2412896b4c7f15b31af09a
Martin <mlgabotto@gmail.com>
2020-12-22 09:39:29 -0300

    Local Storage

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
 
 
-let currTheme = 'white'
+// let currTheme = 'black'
 
 const btnContainer = document.querySelector('#theme');
 const btn = document.querySelector('#btnTheme');
 const title = document.querySelector('h1');
 
 btnContainer.addEventListener('click', x => {
-    changeTheme()
+    displayTheme()
+})
+
+window.addEventListener('DOMContentLoaded', x => {
+    displayTheme();
 })
 
 
-function changeTheme() {
+
+function displayTheme() {
+
+    currTheme = localStorage.getItem('currentTheme')
 
     let style = document.documentElement.style;
     if (currTheme === 'white') {
+        // Define el color de variables css
         style.setProperty('--main-color', 'rgb(255, 255, 255)');
         style.setProperty('--alt-color', 'rgb(0, 0, 0)')
-        currTheme = 'black'
+
         btn.classList.toggle('btnChange')
         title.textContent = 'Dark Theme'
+
+        localStorage.setItem('currentTheme', 'black')
         
     } else {
+        // Define el color de variables css
         style.setProperty('--main-color', 'rgb(0, 0, 0)');
         style.setProperty('--alt-color', 'rgb(255, 255, 255)')
-        currTheme = 'white'
+
         btn.classList.toggle('btnChange')
         title.textContent = 'White Theme'
+
+        localStorage.setItem('currentTheme', 'white')
     }
 }
